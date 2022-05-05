317a3ee2ffb6121310b4b68a5e6478ce2204e827
thaihoc2001 <thaihoc95x@gmail.com>
2022-05-05 22:44:28 +0700

    add event button home

--- app/src/main/java/com/example/quanlynhansu/activity/HomeActivity.java
+++ app/src/main/java/com/example/quanlynhansu/activity/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.quanlynhansu.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.quanlynhansu.R;
 
 public class HomeActivity extends AppCompatActivity {
+    Button btnAdd, btnList;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnList = findViewById(R.id.btnList);
+
+        btnAdd.setOnClickListener((v) -> {
+            showFormAddEmployee();
+        });
+        btnList.setOnClickListener((v) -> {
+            showListEmployee();
+        });
+    }
+    private void showFormAddEmployee() {
+        final Intent intentAddEmployee = new Intent(this,AddEmployeeActivity.class);
+        startActivity(intentAddEmployee);
+    }
+    private void showListEmployee() {
+        final Intent intentListEmployee = new Intent(this,ListEmployeeActivity.class);
+        startActivity(intentListEmployee);
     }
 }
